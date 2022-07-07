9678d193b01d1417a8643cf1639cff157e3e1e17
Felipe Cody <fcody@va-i3.kzn.21-school.ru>
2022-07-07 14:23:19 +0300

    making join #1

--- map_2-3_test.cpp
+++ map_2-3_test.cpp
@@ -4,11 +4,11 @@
 #include <iomanip>
 #include <unistd.h>
 #define RED "\e[0;31m"
-#define BLUE "\e[0;34m"
+#define BLUE "\e[0;36m"
 #define LIGHT_GREY "\e[0;37m"
 #define GREY "\e[0;90m"
 #define WHITE "\e[0;38m"
-#define WHITE_LINE "\e[4;30m"
+#define WHITE_LINE "\e[4;37m"
 #define RED_LINE "\e[4;31m"
 #include <iostream>
 #include <list>
@@ -26,6 +26,141 @@ namespace ft{
 
     };
     class map{
+        public:
+            map(): _root(nullptr), _debug(0){};
+            map(int key, int val) : _debug(0) {
+    //                cout << "create root node " << key <<"\n";
+                createRootNode(key,val);
+            }
+            ~map(){};
+            void    append(int key,int val){
+                if(_debug)
+                    std::cout << RED << ">>> append "<<key << WHITE<<"\n";
+                if(!_root) {
+                    createRootNode(key, val);
+                    return;
+                }
+                node* ptr = _root;
+                node* nodeNew = createNode(key,val);
+                while(1) {
+                    if (nodeNew->key > ptr->key)
+                    {
+                        if(ptr->right && ptr->right->key)
+                            ptr=ptr->right;
+                        else
+                        {
+                            nodeNew->parent = ptr;
+                            ptr->right = nodeNew;
+                            cout << "\nadd " << key << "\n";
+                            balanceAdd(ptr->right);
+                            break;
+                        }
+                    }
+                    else if (nodeNew->key < ptr->key)
+                    {
+                        if(ptr->left && ptr->left->key)
+                            ptr=ptr->left;
+                        else
+                        {
+                            nodeNew->parent = ptr;
+                            ptr->left = nodeNew;
+                            cout << "\nadd " << key << "\n";
+                            balanceAdd(ptr->left);
+                            break;
+                        }
+                    }
+                    else
+                        return;
+                }
+            };
+            void    print(int level, node *ptr)
+            {
+                int i;
+                if (ptr != NULL)
+                {
+                    print(level + 1, ptr->right);
+                    cout << "\n";
+                    for (i = 0; i < level && ptr != _root; i++)
+                        cout << "     ";
+                    if(ptr->color)
+                        printNodeAdr(ptr,1);
+                    else
+                        printNodeAdr(ptr,0);
+                    print(level + 1, ptr->left);
+                }
+            }
+            void    printNodeAdr(node* ptr, bool color){
+                if(_debug)
+                {
+                    cout << BLUE;
+                    if (ptr->parent)
+                        cout << to_string((long) ptr->parent).substr(13, 2);
+                    else
+                        cout << "00";
+                }
+                cout << GREY;
+
+                if(color)
+                    cout << RED_LINE << ptr->key << GREY;
+                else
+                    cout << WHITE_LINE << ptr->key << GREY;
+                if(_debug) {
+                    cout << BLUE << to_string((long) ptr).substr(13, 2) << GREY;
+
+                    if (ptr->left)
+                        cout << to_string((long) ptr->left).substr(13, 2);
+                    else
+                        cout << "00";
+
+                    if (ptr->right)
+                        cout << to_string((long) ptr->right).substr(13, 2);
+                    else
+                        cout << "00";
+                }
+                cout << WHITE;
+            }
+            void    setDebugMode() {_debug = 1;};
+            node*   getRoot(){return _root;}
+            void    erase(int key)
+        {
+            if(_debug) {
+                cout << RED << "\n>>> erase " << key << WHITE << "\n";
+                std::cout << "\n";
+            }
+            node* ptr = findByKey(key);
+            if (!ptr)
+                return;
+            // deleting not leaf node
+            if (ptr->left->key && ptr->right->key) {
+                cout << "erase: left leaf & right leaf: replace successor \n";
+                balanceDelete(replaceSuccessor(ptr));
+            } else {
+//                 deleting inside 3-node
+                if (!ptr->right->key && ptr->left->key && ptr->left->color) {
+                    cout << "erase:"<<RED<<" left"<<WHITE<<" & !right \n";
+                    deleteLeafNode(ptr->right);
+                    ptr->right = nullptr;
+                    ptr->right = ptr->left->right;
+                    ptr->key = ptr->left->key;
+                    ptr->val = ptr->left->val;
+                    deleteLeafNode(ptr->left);
+                    ptr->left = createNullNode(ptr->left);
+                    balanceDelete(ptr);
+                    // deleting leaf node
+                } else if (!ptr->right->key && !ptr->left->key) {
+                    cout << "erase: no left leaf & no right leaf \n";
+                    deleteLeafNode(ptr->left);
+                    deleteLeafNode(ptr->right);
+                    ptr->left = nullptr;
+                    ptr->right = nullptr;
+                    ptr->key = 0;
+                    ptr->val = 0;
+                    ptr->color = 0;
+                    balanceDelete(ptr);
+                }
+            }
+//            print(1,_root);
+        }
         private:
             node*   _root;
             bool _debug;
@@ -68,7 +203,7 @@ namespace ft{
                 nullNode->right = nullptr;
                 return nullNode;
             };
-            void    balance(node* ptr){
+            void    balanceAdd(node* ptr){
                 if(_debug)
                     std::cout << "balance "<<"\n";
                 while(ptr)
@@ -163,6 +298,14 @@ namespace ft{
                 }
                 return res;
             }
+            void    deleteLeafNode(node* ptr){
+            ptr->parent = NULL;
+            ptr->left = NULL;
+            ptr->right = NULL;
+            ptr->color=0;
+            delete ptr;
+            ptr = NULL;
+        }
             node*   replaceSuccessor(node* ptr) {
                 node* succ = ptr;
                 cout << "replace successor: " << succ->key << " to ";
@@ -182,16 +325,16 @@ namespace ft{
                 succ->color = 0;
                 return succ;
             }
-            void    balanceAfterDelete(node* deleted)
+            void    balanceDelete(node* deleted)
             {
                 if(_debug)
-                    cout << "\nbalance after delete "  << "\n";
+                    cout << "\nbalance delete "  << "\n";
                 node* parent = deleted->parent;
                 if (!parent) {
-                    cout << "\n!parent ??? -> done "  << "\n";
+                    cout << "\n!this is root -> done "  << "\n";
                     return;
                 }
-                cout << "balance after delete: balance at " << parent->key << " ";
+                cout << "balance delete: balance at " << parent->key << " ";
                 if (!parent->left->key && !parent->right->key) {
                     cout << "no leaf nodes -> done "  << "\n";
                     return;
@@ -210,118 +353,37 @@ namespace ft{
                 }
                 if (parent->left->key || parent->right->key) {
                     cout << "- right is deleted- left is black -> merge\n";
-                    merge(parent);
-                    balanceAfterDelete(parent);
+                    join(parent);
+                    balanceDelete(parent);
     //                     case 1.2: left Black - merge
                     return;
                 }
             }
-            void    merge(node* ptr){};
-            void    deleteLeafNode(node* ptr){
-                ptr->parent = NULL;
-                ptr->left = NULL;
-                ptr->right = NULL;
-                ptr->color=0;
-                delete ptr;
-                ptr = NULL;
-            }
-
-        public:
-                    map(): _root(nullptr), _debug(0){};
-                    map(int key, int val) : _debug(0) {
-//                cout << "create root node " << key <<"\n";
-                createRootNode(key,val);
-            }
-                    ~map(){};
-            void    append(int key,int val){
-                if(_debug)
-                    std::cout << RED << ">>> append "<<key << WHITE<<"\n";
-                if(!_root) {
-                    createRootNode(key, val);
-                    return;
+            void    join(node* emptyNode){
+                node* a,b,c,d,newEmpty,grandParent,new3black,new3red;
+                short type = 2;
+                // my sibling left or right?
+                if(emptyNode->parent->left && emptyNode->parent->left == emptyNode) {
+                    type = 0;
+                    std::cout << "join: im in left";
                 }
-                node* ptr = _root;
-                node* nodeNew = createNode(key,val);
-                while(1) {
-                    if (nodeNew->key > ptr->key)
-                    {
-                        if(ptr->right && ptr->right->key)
-                            ptr=ptr->right;
-                        else
-                        {
-                            nodeNew->parent = ptr;
-                            ptr->right = nodeNew;
-                            cout << "\nadd " << key << "\n";
-                            balance(ptr->right);
-                            break;
-                        }
+                if(emptyNode->parent->left && emptyNode->parent->right == emptyNode) {
+                    type = 1;
+                    std::cout << "join: im in right";
+                }
+                switch (type) {
+                    // empty is right
+                    case 1: {
+                        printf("\nempty:%llu|gp:%llu|a:%llu|b:%llu|c:%llu|d:%llu|\n",emptyNode,emptyNode->parent,emptyNode->parent->left->left,emptyNode->parent->left->right,emptyNode->left,emptyNode->right);
+                        break;
                     }
-                    else if (nodeNew->key < ptr->key)
-                    {
-                        if(ptr->left && ptr->left->key)
-                            ptr=ptr->left;
-                        else
-                        {
-                            nodeNew->parent = ptr;
-                            ptr->left = nodeNew;
-                            cout << "\nadd " << key << "\n";
-                            balance(ptr->left);
-                            break;
-                        }
+                    // empty is left
+                    case 0: {
+                        break;
                     }
-                    else
-                        return;
+                    default: return;
                 }
             };
-            void    erase(int key){};
-            void    print(int level, node *ptr)
-            {
-                int i;
-                if (ptr != NULL)
-                {
-                    print(level + 1, ptr->right);
-                    cout << "\n";
-                    for (i = 0; i < level && ptr != _root; i++)
-                        cout << "     ";
-                    if(ptr->color)
-                        printNodeAdr(ptr,1);
-                    else
-                        printNodeAdr(ptr,0);
-                    print(level + 1, ptr->left);
-                }
-            }
-            void    printNodeAdr(node* ptr, bool color){
-                if(_debug)
-                {
-                    cout << BLUE;
-                    if (ptr->parent)
-                        cout << to_string((long) ptr->parent).substr(13, 2);
-                    else
-                        cout << "00";
-                }
-                    cout << GREY;
-
-                    if(color)
-                        cout << RED_LINE << ptr->key << GREY;
-                    else
-                        cout << WHITE_LINE << ptr->key << GREY;
-                if(_debug) {
-                    cout << BLUE << to_string((long) ptr).substr(13, 2) << GREY;
-
-                    if (ptr->left)
-                        cout << to_string((long) ptr->left).substr(13, 2);
-                    else
-                        cout << "00";
-
-                    if (ptr->right)
-                        cout << to_string((long) ptr->right).substr(13, 2);
-                    else
-                        cout << "00";
-                }
-                    cout << WHITE;
-            }
-            void    setDebugMode() {_debug = 1;};
-            node*   getRoot(){return _root;}
     };
 }
 
@@ -340,7 +402,7 @@ int main(int argc, char** argv)
     map.append(5,99);
     map.append(6,99);
     map.append(7,99);
-//    map.append(8,99);
+    map.append(8,99);
 //    map.append(9,99);
 //    map.append(10,99);
 //    map.append(11,99);
@@ -359,11 +421,11 @@ int main(int argc, char** argv)
 //    map.append(9,99);
 //    map.append(8,99);
 //    map.erase(13);
-    map.erase(6);
+//    map.erase(6);
 //    map.print(1,map.getRoot());
     map.erase(7);
-//    map.erase(15);
-//    map.print(1,map.getRoot());
+    map.print(1,map.getRoot());
+    map.erase(8);
     return 0;
 }
 
