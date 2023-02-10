6eac4df8d0a695684dc4229e363eb68ae02271cc
Dmitrii Grigorev <vid.kazan@gmail.com>
2023-02-10 11:55:32 +0100

    stack

---
+++ stack.hpp
@@ -0,0 +1,40 @@
+#pragma once
+# include "./vector.hpp"
+namespace ft
+{
+	template< class T, class Container = ft::vector<T> >
+	class stack
+	{
+	public:
+		typedef Container 							container_type;
+		typedef typename Container::value_type		value_type;
+		typedef typename Container::size_type		size_type;
+		typedef typename Container::reference		reference;
+		typedef typename Container::const_reference	const_reference;
+
+	protected:
+		Container	c;
+
+	public:
+		explicit stack(const Container & cont = Container()) : c(cont) {}
+		stack(const stack & copy) : c(copy.c) {}
+		~stack() {}
+		stack &operator=(const stack &other) {c = other.c; return(*this);}
+
+		reference top(void) {return c.back();}
+		const_reference top(void) const {return c.back();}
+
+		bool empty(void) {return c.empty();}
+		size_type size(void) {return c.size();}
+
+		void push(const value_type &val) {c.push_back(val);}
+		void pop(void) {c.pop_back();}
+
+		friend bool operator==(const stack &lhs, const stack &rhs)				{ return lhs.c == rhs.c; };
+		friend bool operator!=(const stack &lhs, const stack &rhs)				{ return lhs.c != rhs.c; };
+		friend bool operator<=(const stack &lhs, const stack &rhs)				{ return lhs.c <= rhs.c; };
+		friend bool operator>=(const stack &lhs, const stack &rhs)				{ return lhs.c >= rhs.c; };
+		friend bool operator<(const stack &lhs, const stack &rhs)				{ return lhs.c < rhs.c; };
+		friend bool operator>(const stack &lhs, const stack &rhs)				{ return lhs.c > rhs.c; };
+	};
+}
